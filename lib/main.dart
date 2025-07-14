1d8cce80a29e6ee3b39379690e84ede0f3b0ef21
Manulik <125672053+Otocolobus-manules@users.noreply.github.com>
2025-07-14 10:33:18 +0300

    feat: app update

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,21 @@ import 'dart:developer';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:bloc/bloc.dart';
 
-import 'src/app.dart';
+import 'package:practice_3_course/src/app.dart';
+import 'package:practice_3_course/src/common/bloc/base_observer.dart';
+import 'package:practice_3_course/src/theme/app_colors.dart';
+
 
 void main() {
+  Bloc.observer = const BaseObserver();
   runZonedGuarded(
       () => runApp(
             DevicePreview(
               enabled: !kReleaseMode,
               builder: (context) => const CoffeeShop(),
+              backgroundColor: AppColors.background,
             ),
           ), (error, stack) {
     log(error.toString(), name: 'App Error', stackTrace: stack);
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,21 +1,67 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:practice_3_course/src/localization/app_localizations.dart';
-
-import 'package:practice_3_course/src/theme/theme.dart';
+import 'package:practice_3_course/src/features/menu/bloc/menu_bloc.dart';
+import 'package:practice_3_course/src/features/menu/data/category_repository.dart';
+import 'package:practice_3_course/src/features/menu/data/data_sources/categories_data_source.dart';
+import 'package:practice_3_course/src/features/menu/data/data_sources/menu_data_source.dart';
+import 'package:practice_3_course/src/features/menu/data/menu_repository.dart';
 import 'package:practice_3_course/src/features/menu/view/menu_screen.dart';
+import 'package:practice_3_course/src/features/order/bloc/order_bloc.dart';
+import 'package:practice_3_course/src/features/order/data/data_sources/order_data_source.dart';
+import 'package:practice_3_course/src/features/order/data/order_repository.dart';
+import 'package:practice_3_course/src/common/extensions/context_extensions.dart';
+import 'package:practice_3_course/src/theme/theme.dart';
 
 
 class CoffeeShop extends StatelessWidget {
   const CoffeeShop({super.key});
 
+  static final dioClient = Dio(
+    BaseOptions(
+      baseUrl: 'https://coffeeshop.academy.effective.band/api/v1',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      connectTimeout: const Duration(seconds: 20),
+      receiveTimeout: const Duration(seconds: 20),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<ICategoryRepository>(
+          create: (_) => CategoriesRepository(
+            networkCategoriesDataSource: NetworkCategoriesDataSource(
+              dio: dioClient,
+            ),
+          ),
+        ),
+        RepositoryProvider<IMenuRepository>(
+          create: (_) => MenuRepository(
+            networkMenuDataSource: NetworkMenuDataSource(
+              dio: dioClient,
+            ),
+          ),
+        ),
+        RepositoryProvider<IOrderRepository>(
+          create: (_) => OrderRepository(
+            networkOrderDataSource: NetworkOrderDataSource(
+              dio: dioClient,
+            ),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
         debugShowCheckedModeBanner: false,
         localizationsDelegates: const [
           AppLocalizations.delegate,
@@ -24,9 +70,25 @@ class CoffeeShop extends StatelessWidget {
           GlobalCupertinoLocalizations.delegate,
         ],
         supportedLocales: AppLocalizations.supportedLocales,
-        onGenerateTitle: (context) => AppLocalizations.of(context)!.title,
+        onGenerateTitle: (context) => context.l10n.title,
         theme: theme,
-        home: const MenuScreen(),
+        home: MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (context) => MenuBloc(
+                menuRepository: context.read<IMenuRepository>(),
+                categoryRepository: context.read<ICategoryRepository>(),
+              ),
+            ),
+            BlocProvider(
+              create: (context) => OrderBloc(
+                orderRepository: context.read<IOrderRepository>(),
+              ),
+            )
+          ],
+          child: const MenuScreen(),
+        ),
+      ),
     );
   }
 }
